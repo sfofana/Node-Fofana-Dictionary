9b6a0b171e29525a705b4245d89f37b4aff49ceb
sfofana <sufyanfofana@yahoo.com>
2020-02-18 17:12:34 -0600

    functioning dictionary search app

--- Node-Fofana Dictionary/src/app/dictionary/dictionary.component.ts
+++ Node-Fofana Dictionary/src/app/dictionary/dictionary.component.ts
@@ -10,15 +10,18 @@ export class DictionaryComponent implements OnInit {
 
   private mockWord = "dictionary";
   private word: string;
+  private display: string;
   private results: Array<any>;
 
   constructor(private service:DictionaryService){ }
 
   ngOnInit() {
     this.service.search(this.mockWord).subscribe(data=>this.results=data[0].shortdef);
+    this.display = this.mockWord;
   }
 
   public search(){
+    this.display = this.word;
     return this.service.search(this.word).subscribe(data=>this.results=data[0].shortdef);
     console.log(this.word);
   }
